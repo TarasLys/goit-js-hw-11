5db424221c8714dae37a9b429afe23b9110a033a
TarasLys <opros0609@gmail.com>
2023-10-25 23:06:44 +0300

    seven

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,26 +1,12 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 axios.defaults.headers.common["x-api-key"] = "40175066-fc06551b58f265feccdc9509e";
 axios.defaults.baseURL = "https://pixabay.com/api/"
 
 
-// export async function getPicture(arr) {
-//     const resps = arr.map(async item => { 
-//         const resp = await fetch(`https://pixabay.com/api/?key=40175066-fc06551b58f265feccdc9509e&q=${item}&image_type=photo&orientation=horizontal&safesearch=true`)
-//         if (!resp.ok) { 
-//             throw new Error()
-//         }
-//         return resp.json()
-//     })
-//     const data = await Promise.allSettled(resps)
-//     const pictureObj = data.filter(({ status }) => status === "fulfilled").map(({ value }) => value);
-//     console.log(pictureObj);
-//     return pictureObj;
-// }
-
-
-// Функція для виконання HTTP-запиту
 export async function fetchImages(query, page = 1) {
   const response = await fetch(`https://pixabay.com/api/?key=40175066-fc06551b58f265feccdc9509e&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`);
   
@@ -34,13 +20,13 @@ const json = await response.json();
     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
     return;
   }
-  
-
-  return json.hits;
+  let images = json.hits
+  let total = json.totalHits
+  return { images, total  };
 }
 
 
 
-// https://pixabay.com/api/?key=40175066-fc06551b58f265feccdc9509e&q=${query}&image_type=photo&orientation=horizontal&safesearch=true
+
 
 
--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import { fetchImages } from './cat-api.js';
 import Notiflix from 'notiflix';
 //import SlimSelect from 'slim-select'
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-// Отримання елементів форми
 const form = document.getElementById('search-form');
 const input = form.elements.searchQuery;
 
-// Отримання елемента галереї
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector(".js-load")
+const total = document.querySelector(".total")
 let currentPage = 1;
 
-// Функція для створення картки зображення
+loadMore.style.display = 'none';
+
 function createCard(images) {
 
 const markup = images.map((image) => `<div class="photo-card">
@@ -31,53 +33,205 @@ const markup = images.map((image) => `<div class="photo-card">
       <b>Downloads</b>${image.downloads}
     </p>
   </div></div>`);
-gallery.insertAdjacentHTML("beforeend", markup.join(""));  
-}
+  gallery.insertAdjacentHTML("beforeend", markup.join(""));  
 
+if (images.length > 20) {
+    loadMore.style.display = 'none';
+  } else {
+    loadMore.style.display = 'block';
+  }
+
+
+new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt'
+});
+}
 
 loadMore.addEventListener("click", onLoad)
 
 function onLoad() {
-  return currentPage += 1;
+return  currentPage += 1;
 }
 
 
-// Обробник події сабміту форми
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
-
-  // Очищення галереї
   gallery.innerHTML = '';
 
   try {
-    // Виконання HTTP-запиту
     if (input.value.length === 0) { 
       Notiflix.Notify.failure('You must enter a request.');
     return;
     }
-  
-    const images = await fetchImages(input.value, currentPage);
-    createCard(images)
+    const result = await fetchImages(input.value, currentPage);
     
+    total.textContent = `Hooray! We found totalHits images - ${result.total} pcs`;
+    createCard(result.images)
 
-
-    // Очищення поля вводу
-    ///input.value = '';
-    
-    // Переміщення фокусу на поле вводу
     input.focus();
-    //return images;
-
-
     
-    } catch (error) {
-      //console.error(error);
+    } catch (error) {  
       Notiflix.Notify.failure('An error occurred while fetching images. Please try again.');
     }
 });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { fetchImages } from './cat-api.js';
+// import Notiflix from 'notiflix';
+// //import SlimSelect from 'slim-select'
+
+
+
+// // const target = document.querySelector(".js-guard")
+// // let options = {
+// //   root: null,
+// //   rootMargin: "200px",
+// //   threshold: 1.0,
+// // };
+
+// // let observer = new IntersectionObserver(onLoad, options);
+
+// // function onLoad(entries, observer) {
+// //   entries.forEach((entry) => {
+// //     if (entry.isIntersecting){
+// //       console.log(entries);
+// //     }
+// //   })
+  
+// // }
+
+
+
+
+// // Отримання елементів форми
+// const form = document.getElementById('search-form');
+// const input = form.elements.searchQuery;
+
+// // Отримання елемента галереї
+// const gallery = document.querySelector('.gallery');
+// const loadMore = document.querySelector(".js-load")
+// let currentPage = 1;
+
+// loadMore.style.display = 'none';
+
+// // Функція для створення картки зображення
+// function createCard(images) {
+
+// const markup = images.map((image) => `<div class="photo-card">
+//   <img src="${image.largeImageURL}" alt="${image.tags}" loading="lazy" width = "350px"/>
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>${image.likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>${image.views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>${image.comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>${image.downloads}
+//     </p>
+//   </div></div>`);
+//   gallery.insertAdjacentHTML("beforeend", markup.join(""));  
+  
+// }
+
+// //loadMore.style.display = 'none';
+// loadMore.addEventListener("click", onLoad)
+
+// function onLoad() {
+  
+//   return currentPage += 1;
+  
+  
+// }
+
+
+
+
+
+
+
+// // Обробник події сабміту форми
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   // Очищення галереї
+//   gallery.innerHTML = '';
+
+//   try {
+    
+//     // Виконання HTTP-запиту
+//     if (input.value.length === 0) { 
+//       Notiflix.Notify.failure('You must enter a request.');
+//     return;
+//     }
+
+   
+
+//   //   if (json.hits !== json.totalHits)
+//   // { 
+//   //   loadMore.style.display = 'display';
+//   // }
+  
+//     const images = await fetchImages(input.value, currentPage);
+
+//     createCard(images)
+//     //observer.observe(target);
+   
+
+
+//     // Очищення поля вводу
+//     ///input.value = '';
+    
+//     // Переміщення фокусу на поле вводу
+//     input.focus();
+//     //return images;
+
+
+    
+//     } catch (error) {
+//       //console.error(error);
+//       Notiflix.Notify.failure('An error occurred while fetching images. Please try again.');
+//     }
+// });
+
+
+
+// // let counter = 0;
+// // document.addEventListener("scroll", onScroll)
+
+// // function onScroll() {
+// //   counter += 1
+// //   console.log(counter)
+// // }
 
 
 
